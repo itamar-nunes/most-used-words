f0b469202d02780313279723f3966c3d14d5241a
Itamar Nunes <itamarbel@gmail.com>
2020-05-05 00:03:52 -0300

    New components - Home.vue and Pill.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark></v-app-bar>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Most Used Words</v-toolbar-title>
+    </v-app-bar>
 
-    <v-content></v-content>
+    <v-content>
+      <Home />
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import Home from "./components/Home";
+
 export default {
   name: "App",
 
-  components: {},
+  components: {
+    Home
+  },
 
   data: () => ({
     //
---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill";
+
+export default {
+  name: "Home",
+  components: {
+    Pill
+  },
+  data: function() {
+    return {
+      groupedWords: [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "youuuuu", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
