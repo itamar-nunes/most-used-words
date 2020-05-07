de9c493b26384988d277a0f3e51b314300f1a2c9
Itamar Nunes <itamarbel@gmail.com>
2020-05-06 22:40:34 -0300

    Add function to click button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,16 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        multiple 
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles"
+      />
+    </v-form>
     <div class="pills">
       <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
     </div>
@@ -16,27 +27,18 @@ export default {
   },
   data: function() {
     return {
+      files: [],
       groupedWords: [
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
-        { name: "i", amount: 1234 },
-        { name: "youuuuu", amount: 900 },
-        { name: "he", amount: 853 },
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 853 },
         { name: "i", amount: 1234 },
         { name: "you", amount: 900 },
         { name: "he", amount: 853 }
       ]
     };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    }
   }
 };
 </script>
